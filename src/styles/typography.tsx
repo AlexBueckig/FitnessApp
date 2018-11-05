b03b7faaad88d351758e1fb28b6d11cd0835f2e3
Alexander Bueckig <AlexBueckig@gmail.com>
2018-11-05 09:19:20 +0100

    Buttoncolor eingefügt

--- src/styles/typography.tsx
+++ src/styles/typography.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const fontColor = '#000000';
+const fontColorButton = 'green';
 export const opacityPrimary = 0.87;
 export const opacitySecondary = 0.54;
 export const opacityDisabled = 0.38;
@@ -12,7 +13,7 @@ const typography = StyleSheet.create({
   body: { fontSize: 14, fontFamily: 'sans-serif', color: fontColor, opacity: opacityPrimary },
   menu: { fontSize: 14, fontFamily: 'sans-serif-medium', color: fontColor, opacity: opacityPrimary },
   caption: { fontSize: 12, fontFamily: 'sans-serif', color: fontColor, opacity: 1 },
-  button: { fontSize: 14, fontFamily: 'sans-serif-medium', color: fontColor, opacity: opacityPrimary },
+  button: { fontSize: 14, fontFamily: 'sans-serif-medium', color: fontColorButton, opacity: opacityPrimary },
   label: { color: '#86939e', fontSize: 16, fontFamily: 'sans-serif', fontWeight: 'bold' }
 });
 
