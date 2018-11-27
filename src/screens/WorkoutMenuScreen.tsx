70a94ccd04ff7cac534bd8f3ad250c2825b7044a
Alexander Bueckig <AlexBueckig@gmail.com>
2018-11-27 12:07:40 +0100

    Menü Design implementiert

--- src/screens/WorkoutMenuScreen.tsx
+++ src/screens/WorkoutMenuScreen.tsx
@@ -1,15 +1,30 @@
 import React, { PureComponent } from 'react';
-import { View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { ScrollView, Text } from 'react-native';
 import { Navigation } from 'react-native-navigation';
+import CardImageBackground from '../components/CardImageBackground';
+
+import styles from '../styles';
 
 interface IProps {
   componentId: string;
 }
 
 export default class WorkoutMenuScreen extends PureComponent<IProps> {
+  static get options() {
+    return {
+      topBar: {
+        title: { text: 'Workoutmenü' }
+      }
+    };
+  }
+
+  private images = [
+    require('../../res/images/download2.jpg'),
+    require('../../res/images/ausrustung-crossfit-fitness-669578-2.jpg'),
+    require('../../res/images/anstrengung-ausbildung-beine-841125-2.jpg')
+  ];
+
   public onPress(target: string) {
-    console.log('Button pressed');
     Navigation.push(this.props.componentId, {
       component: {
         name: target
@@ -19,11 +34,21 @@ export default class WorkoutMenuScreen extends PureComponent<IProps> {
 
   public render() {
     return (
-      <View>
-        <Button onPress={this.onPress.bind(this, 'WorkoutScreen')} title="WorkoutScreen" />
-        <Button onPress={this.onPress.bind(this, 'DayScreen')} title="DayScreen" />
-        <Button onPress={this.onPress.bind(this, 'ExerciseScreen')} title="ExerciseScreen" />
-      </View>
+      <ScrollView style={styles.layout.main}>
+        <CardImageBackground
+          title="Trainingspläne"
+          onPress={this.onPress.bind(this, 'WorkoutScreen')}
+          image={this.images[0]}
+        >
+          <Text style={styles.typography.cardBody}>Füge Tagespläne zu Trainingsplänen zusammen</Text>
+        </CardImageBackground>
+        <CardImageBackground title="Tagespläne" onPress={this.onPress.bind(this, 'DayScreen')} image={this.images[1]}>
+          <Text style={styles.typography.cardBody}>Erstelle einzelne, individuelle Tagespläne</Text>
+        </CardImageBackground>
+        <CardImageBackground title="Übungen" onPress={this.onPress.bind(this, 'ExerciseScreen')} image={this.images[2]}>
+          <Text style={styles.typography.cardBody}>Lege deine eigenen Übungen an</Text>
+        </CardImageBackground>
+      </ScrollView>
     );
   }
 }
