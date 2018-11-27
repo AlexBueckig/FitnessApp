20aecc5d894cab24c534707a72e9ddf2f93c1ccb
Alexander Bueckig <AlexBueckig@gmail.com>
2018-11-27 12:05:11 +0100

    isFetching aus ListEmptyComponent entfernt

--- src/components/ListComponents.tsx
+++ src/components/ListComponents.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Divider } from 'react-native-elements';
 import styles from '../styles';
 
@@ -8,17 +8,17 @@ export const ListItemSeperator = () => {
   // return <View style={{ height: 1, backgroundColor: 'black', marginVertical: 10 }} />;
 };
 
-export const ListEmptyComponent = (isFetching: boolean) => {
-  if (!isFetching) {
-    return (
-      <View>
-        <Text style={styles.typography.body}>Du hast noch keine Trainigspläne angelegt. </Text>
-        <Text style={styles.typography.body}>
-          Wenn du eigene Pläne erstellen willst, drücke einfach unten rechts auf das + Symbol.
-        </Text>
-      </View>
-    );
-  } else {
-    return <ActivityIndicator size="large" />;
-  }
+export const SectionSeperator = () => {
+  return <Divider style={{ height: 2 }} />;
+};
+
+export const ListEmptyComponent = () => {
+  return (
+    <View>
+      <Text style={styles.typography.body}>Du hast noch keine Trainigspläne angelegt. </Text>
+      <Text style={styles.typography.body}>
+        Wenn du eigene Pläne erstellen willst, drücke einfach unten rechts auf das + Symbol.
+      </Text>
+    </View>
+  );
 };
