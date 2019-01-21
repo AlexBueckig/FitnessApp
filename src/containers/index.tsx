e58fb71003c8a1169f0fde762b973fff65bdefb1
Alexander Bückig <AlexBueckig@gmail.com>
2019-01-21 10:51:08 +0100

    removed unneccesary components

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -2,10 +2,7 @@ import { Navigation } from 'react-native-navigation';
 import { Provider } from 'react-redux';
 import { Store } from 'redux';
 
-import DeleteButton from '../components/DeleteButton';
 import AchievementScreen from '../screens/AchievementScreen';
-import CalendarScreen from '../screens/CalendarScreen';
-import SecondScreen from '../screens/SecondScreen';
 import WorkoutMenuScreen from '../screens/WorkoutMenuScreen';
 import IStoreState from '../types';
 import DayAddContainer from './DayAddContainer';
@@ -19,9 +16,9 @@ import WorkoutContainer from './WorkoutContainer';
 export const registerScreens = (store: Store<IStoreState>) => {
   Navigation.registerComponent('WorkoutMenuScreen', () => WorkoutMenuScreen);
   Navigation.registerComponent('AchievementScreen', () => AchievementScreen);
-  Navigation.registerComponent('SecondScreen', () => SecondScreen);
-  Navigation.registerComponent('CalendarScreen', () => CalendarScreen);
-  Navigation.registerComponent('buttons.delete', () => DeleteButton);
+  // Navigation.registerComponent('SecondScreen', () => SecondScreen);
+  // Navigation.registerComponent('CalendarScreen', () => CalendarScreen);
+  // Navigation.registerComponent('buttons.delete', () => DeleteButton);
   Navigation.registerComponentWithRedux('FeedScreen', () => FeedContainer, Provider, store);
   Navigation.registerComponentWithRedux('WorkoutScreen', () => WorkoutContainer, Provider, store);
   Navigation.registerComponentWithRedux('WorkoutScreen.Add', () => WorkoutAddContainer, Provider, store);
