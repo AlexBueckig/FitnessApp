9a08e1480a28bc8645ecf94db24a54e2e940a575
Alexander Bückig <AlexBueckig@gmail.com>
2019-01-22 17:30:18 +0100

    Added getExercises

--- src/containers/DayAddContainer.tsx
+++ src/containers/DayAddContainer.tsx
@@ -1,19 +1,23 @@
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { deleteDay, getDayById, saveDay } from '../redux/actions/dayActions';
+import { getExercises } from '../redux/actions/exerciseActions';
 import DayAddScreen from '../screens/DayAddScreen';
 import IStoreState from '../types';
 import { IDay, IDeleteDay, IGetDayById, ISaveDay } from '../types/dayTypes';
+import { IGetExercises } from '../types/exerciseTypes';
 
 export const mapStateToProps = (state: IStoreState) => ({
   day: state.daysState.currentDay,
+  exercises: state.exercisesState.exercises,
   isFetching: state.daysState.isFetching
 });
 
-export const mapDispatchToProps = (dispatch: Dispatch<ISaveDay | IGetDayById | IDeleteDay>) => ({
+export const mapDispatchToProps = (dispatch: Dispatch<ISaveDay | IGetDayById | IDeleteDay | IGetExercises>) => ({
   saveDay: (day: IDay) => dispatch(saveDay(day)),
   getDayById: (id: number) => dispatch(getDayById(id)),
-  deleteDay: (id: number) => dispatch(deleteDay(id))
+  deleteDay: (id: number) => dispatch(deleteDay(id)),
+  getExercises: () => dispatch(getExercises())
 });
 
 export default connect(
