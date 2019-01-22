006e2dcf8142012fb31d4a91d6d1732a5f91530d
Alexander Bückig <AlexBueckig@gmail.com>
2019-01-22 17:29:43 +0100

    moved shemaVersion to different file because cyclic import warning

--- src/api/realm/index.tsx
+++ src/api/realm/index.tsx
@@ -2,6 +2,4 @@ import * as day from './dayRealm';
 import * as exercise from './exerciseRealm';
 import * as workout from './workoutRealm';
 
-export const SCHEMA_VERSION = 1;
-
 export default { workout, day, exercise };
