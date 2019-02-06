d96f610b1c71369031a3c4ac2a940731cfcbf757
Alexander Bückig <AlexBueckig@gmail.com>
2019-02-06 13:06:03 +0100

    added toasts to state

--- src/redux/reducers/index.tsx
+++ src/redux/reducers/index.tsx
@@ -4,13 +4,15 @@ import IStoreState from '../../types';
 import DayReducer from './DayReducer';
 import ExerciseReducer from './ExerciseReducer';
 import FeedReducer from './FeedReducer';
+import ToastReducer from './ToastReducer';
 import WorkoutReducer from './WorkoutReducer';
 
 const rootReducer: Reducer<IStoreState> = combineReducers({
   feedState: FeedReducer,
   workoutsState: WorkoutReducer,
   daysState: DayReducer,
-  exercisesState: ExerciseReducer
+  exercisesState: ExerciseReducer,
+  toastState: ToastReducer
 });
 
 export default rootReducer;
