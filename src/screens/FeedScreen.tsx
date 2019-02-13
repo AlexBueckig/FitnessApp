dffe44b1789cf54a14214a65073f1ac42ed4bbc8
Alexander Bückig <AlexBueckig@gmail.com>
2019-02-13 11:53:57 +0100

    Animated FeedScreen

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
-import { Dimensions, FlatList, ImageBackground, ScrollView, View } from 'react-native';
+import { Animated, FlatList } from 'react-native';
 import { Divider, ListItem, Text } from 'react-native-elements';
 import { Navigation } from 'react-native-navigation';
+import ImageQuoteCard from '../components/ImageQuoteCard';
 import { IGetFeed, IPost } from '../types/feedTypes';
 
 interface IProps {
@@ -10,7 +11,13 @@ interface IProps {
   getPosts: () => IGetFeed;
 }
 
-export default class FeedScreen extends PureComponent<IProps> {
+interface IState {
+  offsetY: Animated.Value;
+}
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+export default class FeedScreen extends PureComponent<IProps, IState> {
   public static options() {
     return {
       topBar: {
@@ -22,6 +29,7 @@ export default class FeedScreen extends PureComponent<IProps> {
   constructor(props: IProps) {
     super(props);
     Navigation.events().bindComponent(this);
+    this.state = { offsetY: new Animated.Value(0) };
   }
 
   public componentDidAppear() {
@@ -30,41 +38,64 @@ export default class FeedScreen extends PureComponent<IProps> {
 
   public render() {
     return (
-      <ScrollView>
-        <ImageBackground
-          source={require('../../res/images/athlet-ausbildung-ausrustungen-305239.jpg')}
-          style={{ height: 200, width: Dimensions.get('window').width }}
-          resizeMode={'cover'}
-        >
-          <View
-            style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', flex: 1, justifyContent: 'center', paddingHorizontal: 32 }}
-          >
-            <Text style={{ color: '#EEEEEE', fontSize: 22, lineHeight: 36, fontStyle: 'italic' }}>
-              "If you have dreams it is your responsibility to make them happen."
-            </Text>
-            <Text style={{ color: '#CCCCCC', alignSelf: 'flex-end', marginRight: 32 }}>- Bel Pesce</Text>
-          </View>
-        </ImageBackground>
-        <FlatList
-          ListHeaderComponent={() => (
-            <Text style={{ padding: 16, backgroundColor: '#53D0A4', color: 'white', fontSize: 16 }}>
-              Meine kommenden Workouts
-            </Text>
-          )}
-          data={this.props.posts}
-          renderItem={(item: { item: IPost }) => {
-            return (
-              <ListItem
-                title={`Montag, ${item.item.key}. Januar`}
-                subtitle="2er Split - Oberkörper"
-                chevron={{ name: 'chevron-right', size: 26 }}
-              />
-            );
-          }}
-          ItemSeparatorComponent={Divider}
-          keyExtractor={(item: IPost) => JSON.stringify(item.key)}
-        />
-      </ScrollView>
+      <AnimatedFlatList
+        ListHeaderComponent={this.listHeaderComponent}
+        data={this.props.posts}
+        renderItem={(item: { item: IPost }) => {
+          return (
+            <ListItem
+              title={`Montag, ${item.item.key}. Januar`}
+              subtitle="2er Split - Oberkörper"
+              chevron={{ name: 'chevron-right', size: 26 }}
+            />
+          );
+        }}
+        ItemSeparatorComponent={Divider}
+        keyExtractor={(item: IPost) => JSON.stringify(item)}
+        stickyHeaderIndices={[0]}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: this.state.offsetY } } }], {
+          useNativeDriver: true
+        })}
+      />
     );
   }
+
+  private listHeaderComponent = () => (
+    <Animated.View
+      style={{
+        transform: [
+          {
+            translateY: this.state.offsetY.interpolate({
+              inputRange: [0, 200],
+              outputRange: [0, -200],
+              extrapolateRight: 'clamp'
+            })
+          }
+        ]
+      }}
+    >
+      <Animated.View
+        style={{
+          transform: [
+            {
+              scale: this.state.offsetY.interpolate({
+                inputRange: [-200, 0, 1],
+                outputRange: [1.4, 1, 1]
+              })
+            }
+          ],
+          opacity: this.state.offsetY.interpolate({
+            inputRange: [0, 200],
+            outputRange: [1, 0],
+            extrapolateRight: 'clamp'
+          })
+        }}
+      >
+        <ImageQuoteCard />
+      </Animated.View>
+      <Text style={{ padding: 16, backgroundColor: '#53D0A4', color: 'white', fontSize: 16 }}>
+        Meine kommenden Workouts
+      </Text>
+    </Animated.View>
+  );
 }
