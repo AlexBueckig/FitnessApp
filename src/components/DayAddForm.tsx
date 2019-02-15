227cb49885ceca9000b9729043c722c6aa1506f5
Alexander Bückig <AlexBueckig@gmail.com>
2019-02-15 20:26:14 +0100

    Day selection added

--- src/components/DayAddForm.tsx
+++ src/components/DayAddForm.tsx
@@ -2,9 +2,11 @@ import { FieldArray, Formik, FormikProps } from 'formik';
 import React, { Fragment, PureComponent } from 'react';
 import { Picker, View } from 'react-native';
 import { Button, Text } from 'react-native-elements';
+import * as yup from 'yup';
 import { IDay } from '../types/dayTypes';
 import { IExercises } from '../types/exerciseTypes';
-import TextInput from './Input';
+import TextInput from './FormComponents/Input';
+import MultiPicker from './FormComponents/MultiPicker';
 
 interface IProps {
   day: IDay;
@@ -17,6 +19,20 @@ interface IState {
   selectedExercise: number;
 }
 
+const items = [
+  { id: 0, name: 'Montag' },
+  { id: 1, name: 'Dienstag' },
+  { id: 2, name: 'Mittwoch' },
+  { id: 3, name: 'Donnerstag' },
+  { id: 4, name: 'Freitag' },
+  { id: 5, name: 'Samstag' },
+  { id: 6, name: 'Sonntag' }
+];
+
+const validationSchema = yup.object().shape({
+  description: yup.string().required()
+});
+
 class DayAddForm extends PureComponent<IProps, IState> {
   public state = { selectedCategory: 0, selectedExercise: 0 };
 
@@ -26,30 +42,20 @@ class DayAddForm extends PureComponent<IProps, IState> {
       <Formik
         initialValues={{
           id: props.day.id,
-          description: props.day.description,
-          day: props.day.day,
-          sets: props.day.sets
+          description: props.day.description || '',
+          days: props.day.days || [],
+          sets: props.day.sets || []
         }}
         onSubmit={props.handleSubmit}
         enableReinitialize={true}
+        validationSchema={validationSchema}
       >
-        {({
-          values,
-          setFieldValue,
-          setFieldTouched,
-          submitForm,
-          isValid,
-          isSubmitting,
-          errors,
-          touched
-        }: FormikProps<IDay>) => (
+        {({ values, submitForm, isSubmitting, errors, touched }: FormikProps<IDay>) => (
           <View style={{ alignItems: 'center' }}>
             <TextInput
               label="description"
               name="description"
               value={values.description}
-              onChange={setFieldValue}
-              onTouch={setFieldTouched}
               error={touched.description && errors.description ? errors.description : undefined}
             />
             <FieldArray
@@ -72,47 +78,55 @@ class DayAddForm extends PureComponent<IProps, IState> {
                       >
                         {props.exercises.count > 0 &&
                           props.exercises.results[state.selectedCategory].data.map((exercise, index) => (
-                            <Picker.Item key={JSON.stringify(exercise)} value={index} label={exercise.name} />
+                            <Picker.Item
+                              key={`${index}${JSON.stringify(exercise)}`}
+                              value={index}
+                              label={exercise.name}
+                            />
                           ))}
                       </Picker>
+                      <Button
+                        title="Übung hinzufügen"
+                        onPress={() =>
+                          arrayHelpers.push({
+                            id: 0,
+                            sets: 1,
+                            exercise: props.exercises.results[state.selectedCategory].data[state.selectedExercise]
+                          })
+                        }
+                        containerStyle={{ height: 40 }}
+                        buttonStyle={{ backgroundColor: 'rgba(78, 116, 289, 1)' }}
+                        titleStyle={{ color: 'white', marginHorizontal: 20 }}
+                      />
                     </Fragment>
                   )}
-                  <Button
-                    title="Übung hinzufügen"
-                    onPress={() =>
-                      arrayHelpers.push({
-                        id: 0,
-                        sets: 1,
-                        exercise: props.exercises.results[state.selectedCategory].data[state.selectedExercise]
-                      })
-                    }
-                    containerStyle={{ height: 40 }}
-                    buttonStyle={{ backgroundColor: 'rgba(78, 116, 289, 1)' }}
-                    titleStyle={{ color: 'white', marginHorizontal: 20 }}
-                  />
-                  {values.sets.length > 0 &&
-                    values.sets.map((set, index) => {
-                      if (set.exercise) {
-                        return (
-                          <View
-                            key={`${index}-${JSON.stringify(set)}`}
-                            style={{ flexDirection: 'row', alignItems: 'center' }}
-                          >
-                            <Text style={{ flex: 1 }}>{set.exercise.name}</Text>
-                            <Button style={{ height: 32 }} title="Delete" onPress={() => arrayHelpers.remove(index)} />
-                          </View>
-                        );
-                      } else {
-                        return null;
-                      }
-                    })}
+                  {props.exercises && props.exercises.count === 0 && (
+                    <Text>Keine Übungen vorhanden, lege welche an!</Text>
+                  )}
                 </View>
               )}
             />
+            {/*             {values.sets.length > 0 &&
+              values.sets.map((set, index) => {
+                if (set.exercise) {
+                  return (
+                    <View
+                      key={`${index}-${JSON.stringify(set)}`}
+                      style={{ flexDirection: 'row', alignItems: 'center' }}
+                    >
+                      <Text style={{ flex: 1 }}>{set.exercise.name}</Text>
+                      <Button style={{ height: 32 }} title="Delete" onPress={() => arrayHelpers.remove(index)} />
+                    </View>
+                  );
+                } else {
+                  return null;
+                }
+              })} */}
+            <MultiPicker label="Tage" name="days" items={items} selectedItems={values.days} />
             <Button
               title="Submit"
               onPress={submitForm}
-              disabled={!isValid || isSubmitting}
+              disabled={isSubmitting}
               containerStyle={{ height: 40 }}
               buttonStyle={{ backgroundColor: 'rgba(78, 116, 289, 1)' }}
               titleStyle={{ color: 'white', marginHorizontal: 20 }}
