d5589930fec70930d3342a447f58a1e27e70bd7d
Alexander Bückig <AlexBueckig@gmail.com>
2019-02-21 14:19:19 +0100

    added typography

--- src/styles/typography.tsx
+++ src/styles/typography.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 const fontColor = '#000000';
-const fontColorButton = 'green';
 export const opacityPrimary = 0.87;
 export const opacitySecondary = 0.54;
 export const opacityDisabled = 0.38;
@@ -13,10 +12,11 @@ const typography = StyleSheet.create({
   body: { fontSize: 14, fontFamily: 'sans-serif', color: fontColor, opacity: opacityPrimary },
   menu: { fontSize: 14, fontFamily: 'sans-serif-medium', color: fontColor, opacity: opacityPrimary },
   caption: { fontSize: 12, fontFamily: 'sans-serif', color: fontColor, opacity: 1 },
-  button: { fontSize: 14, fontFamily: 'sans-serif-medium', color: fontColorButton, opacity: opacityPrimary },
-  label: { color: '#86939e', fontSize: 16, fontFamily: 'sans-serif', fontWeight: 'bold' },
+  label: { fontSize: 12, fontFamily: 'sans-serif', color: fontColor, opacity: opacitySecondary, fontWeight: 'normal' },
   cardTitle: { fontSize: 24, color: '#FFFFFF', fontFamily: 'sans-serif', elevation: 1, textAlign: 'center' },
-  cardBody: { fontSize: 16, color: '#FFFFFF', fontFamily: 'sans-serif', textAlign: 'center', elevation: 1 }
+  cardBody: { fontSize: 16, color: '#FFFFFF', fontFamily: 'sans-serif', textAlign: 'center', elevation: 1 },
+  listItemTitle: { fontSize: 16, color: fontColor, fontFamily: 'sans-serif', opacity: opacityPrimary },
+  listItemSubtitle: { fontSize: 14, color: fontColor, fontFamily: 'sans-serif', opacity: opacitySecondary }
 });
 
 export default typography;
