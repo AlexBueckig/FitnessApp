4fe979a7f3340ea95fbbfdc9a1b05c887d665447
Alexander Bückig <AlexBueckig@gmail.com>
2019-03-11 11:07:20 +0100

    registered new screens

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -1,28 +1,45 @@
-import React, { ComponentClass, SFC } from 'react';
+import React, { FC } from 'react';
 import { Navigation } from 'react-native-navigation';
+import Modal from '../modals/Modal';
+import WorkoutAddModal from '../modals/WorkoutAdd/WorkoutAddModal';
 import AchievementScreen from '../screens/AchievementScreen';
 import WorkoutMenuScreen from '../screens/WorkoutMenuScreen';
 import DBProvider from '../watermelondb/DBProvider';
+import EnhancedDayAddContainer, { DayAddContainer } from './DayAddContainer';
 import ExerciseAddContainer from './ExerciseAddContainer';
 import EnhancedExerciseContainer, { ExerciseContainer } from './ExerciseContainer';
 import EnhancedExerciseEditContainer, { ExerciseEditContainer } from './ExerciseEditContainer';
+import WorkoutAddContainer from './WorkoutAddContainer';
+import EnhancedWorkoutContainer, { WorkoutContainer } from './WorkoutContainer';
+import EnhancedWorkoutEditContainer, { WorkoutEditContainer } from './WorkoutEditContainer';
 
-function DBWrapper<P = {}>(Screen: ComponentClass<P>) {
-  const EnhancedComponent: SFC<P> = props => (
+const DBWrapper = (Screen: any) => {
+  const EnhancedComponent: FC<any> = props => (
     <DBProvider>
       <Screen {...props} />
     </DBProvider>
   );
 
   return EnhancedComponent;
-}
+};
+
+const ModalWrapper = (Screen: any) => {
+  const EnhancedComponent: FC<any> = props => <Modal render={() => <Screen {...props} />} />;
+
+  return EnhancedComponent;
+};
 
 export const registerScreens = () => {
   Navigation.registerComponent('WorkoutMenuScreen', () => WorkoutMenuScreen);
   Navigation.registerComponent('AchievementScreen', () => AchievementScreen);
   Navigation.registerComponent('FeedScreen', () => WorkoutMenuScreen);
-  Navigation.registerComponent('WorkoutScreen', () => WorkoutMenuScreen);
-  Navigation.registerComponent('WorkoutScreen.Add', () => WorkoutMenuScreen);
+  Navigation.registerComponent('WorkoutScreen', () => DBWrapper(EnhancedWorkoutContainer), () => WorkoutContainer);
+  Navigation.registerComponent('WorkoutScreen.Add', () => WorkoutAddContainer);
+  Navigation.registerComponent(
+    'WorkoutScreen.Edit',
+    () => DBWrapper(EnhancedWorkoutEditContainer),
+    () => WorkoutEditContainer
+  );
   Navigation.registerComponent('DayScreen', () => WorkoutMenuScreen);
   Navigation.registerComponent('DayScreen.Add', () => WorkoutMenuScreen);
   Navigation.registerComponent('ExerciseScreen', () => DBWrapper(EnhancedExerciseContainer), () => ExerciseContainer);
@@ -32,4 +49,10 @@ export const registerScreens = () => {
     () => ExerciseEditContainer
   );
   Navigation.registerComponent('ExerciseScreen.Add', () => ExerciseAddContainer);
+  Navigation.registerComponent(
+    'DayAddModal',
+    () => ModalWrapper(DBWrapper(EnhancedDayAddContainer)),
+    () => DayAddContainer
+  );
+  Navigation.registerComponent('WorkoutAddModal', () => WorkoutAddModal);
 };
