0a58b25c0dd087e9afbb2397ce956babf9d19126
Alexander Bückig <AlexBueckig@gmail.com>
2019-03-15 11:49:28 +0100

    implemented DayAddExerciseModal

---
+++ src/modals/DayAddExercise/DayAddExerciseModalContainer.tsx
@@ -0,0 +1,35 @@
+import { DatabaseProviderProps, withDatabase } from '@nozbe/watermelondb/DatabaseProvider';
+import withObservables from '@nozbe/with-observables';
+import React, { Component } from 'react';
+import DayAddExerciseForm from '../../components/DayAddExerciseForm';
+import Day from '../../watermelondb/models/Day';
+import Exercise from '../../watermelondb/models/Exercise';
+import { ISaveSetParams } from '../../watermelondb/models/Set';
+
+interface IProps {
+  closeModal: () => void;
+  id: string;
+  day?: Day;
+  exercises?: Exercise[];
+}
+
+export class DayAddExerciseModalContainer extends Component<IProps> {
+  saveSet = async (set: ISaveSetParams) => {
+    await this.props.day!.addSet(set);
+    this.props.closeModal();
+  };
+
+  render() {
+    return <DayAddExerciseForm submit={this.saveSet} exercises={this.props.exercises!} />;
+  }
+}
+
+const enhance = withObservables(['id'], ({ id, database }: IProps & DatabaseProviderProps) => ({
+  day: database.collections.get('days').findAndObserve(id!),
+  exercises: database.collections
+    .get('exercises')
+    .query()
+    .observe()
+}));
+
+export default withDatabase<IProps>(enhance(DayAddExerciseModalContainer));
