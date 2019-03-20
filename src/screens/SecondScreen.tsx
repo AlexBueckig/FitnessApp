40f356c6799a5502192371659a8f2633db5af088
Alexander Bückig <AlexBueckig@gmail.com>
2019-03-20 11:43:59 +0100

    deleted

--- src/screens/SecondScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from 'react';
-import { Text } from 'react-native';
-
-export default class SecondScreen extends PureComponent {
-  public render() {
-    return <Text>Second View</Text>;
-  }
-}
