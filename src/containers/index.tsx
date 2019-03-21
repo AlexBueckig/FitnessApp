a89fb23bdc9da25cf7f561d9874788d8654f0426
Alexander Bückig <AlexBueckig@gmail.com>
2019-03-21 13:08:37 +0100

    registered WorkoutAddModal

--- src/containers/index.tsx
+++ src/containers/index.tsx
@@ -3,6 +3,7 @@ import { Navigation } from 'react-native-navigation';
 import DayAddModal from '../modals/DayAdd/DayAddModal';
 import DayAddExerciseModal from '../modals/DayAddExercise/DayAddExerciseModal';
 import WorkoutAddModal from '../modals/WorkoutAdd/WorkoutAddModal';
+import WorkoutEditModal from '../modals/WorkoutEdit/WorkoutEditModal';
 import DBProvider from '../watermelondb/DBProvider';
 import EnhancedDayEditContainer, { DayEditContainer } from './DayEditContainer';
 import ExerciseAddContainer from './ExerciseAddContainer';
@@ -45,5 +46,6 @@ export const registerScreens = () => {
   Navigation.registerComponent('ExerciseScreen.Add', () => ExerciseAddContainer);
   Navigation.registerComponent('DayAddModal', () => DBWrapper(DayAddModal), () => DayAddModal);
   Navigation.registerComponent('WorkoutAddModal', () => WorkoutAddModal);
+  Navigation.registerComponent('WorkoutEditModal', () => DBWrapper(WorkoutEditModal), () => WorkoutEditModal);
   Navigation.registerComponent('DayAddExerciseModal', () => DBWrapper(DayAddExerciseModal), () => DayAddExerciseModal);
 };
