af41c97f9ecac907f46ab8fdf697b8842a714d48
Alexander Bückig <AlexBueckig@gmail.com>
2019-03-21 13:02:47 +0100

    AsyncStorage added

--- android/app/src/main/java/com/fitness/MainApplication.java
+++ android/app/src/main/java/com/fitness/MainApplication.java
@@ -8,13 +8,13 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-
 import java.util.Arrays;
 import java.util.List;
 
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
+import com.reactnativenavigation.react.ReactGateway;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.nozbe.watermelondb.WatermelonDBPackage;
@@ -41,6 +41,7 @@ public class MainApplication extends NavigationApplication {
         // Add additional packages you require here
         // No need to add RnnPackage and MainReactPackage
         return Arrays.<ReactPackage>asList(
+            new AsyncStoragePackage(),
             new SvgPackage(),
             new VectorIconsPackage(),
             new WatermelonDBPackage()
